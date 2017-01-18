8572e427aaa7e63cee07fc2a851d1b00d5cd592a
lishuo <lishuo98@yahoo.com.sg>
2017-01-18 15:53:50 +0800

    Fixed memory leak

--- make_xml.py
+++ make_xml.py
@@ -3,42 +3,54 @@ from pprint import pprint
 
 def make_xml(length):
 	i = 0
-	counter = 0
-	for event, elem in etree.iterparse('../doc_similarity/enwiki-latest-pages-articles.xml', events=('end',)):
+	for event, elem in etree.iterparse('../doc_similarity/enwiki-latest-pages-articles.xml', events=('end', )):
 	
 		try:
 			if i < length:
-				if event == 'end': 
-					if elem.tag == '{http://www.mediawiki.org/xml/export-0.10/}text':
-						if len(elem.text) > 200:
-					
-							#if counter != step:
-								#counter += 1
-								#elem.clear()
-							
-							#elif counter == step:
-							i += 1
-								#counter = 0
-							yield elem.text
-							elem.clear()
-						
-			elif i >= length:
+				if event == 'end' and \
+					elem.tag == '{http://www.mediawiki.org/xml/export-0.10/}text' and \
+					len(elem.text) > 200:
+
+					i += 1
+					yield elem.text
+				
+			else:
 				break
 		except:
-			continue
+			pass
+		elem.clear()
 			
 			
 if __name__ == '__main__':
-	a = input('Number of texts: ')
-	b = input('Step seperation: ')
+	a = int(input('Number of texts: '))
+	b = int(input('Step seperation: '))
+	c = a*b
 	
-	c = int(a)*int(b)
 	root = etree.Element('texts')
+	
 	for i, t in enumerate(make_xml(c)):
-		doc = etree.SubElement(root, 'text')
-		#doc.text = t
-		if i % 300 == 0:
-			print(i/300)
+		if i % b == 0:
+			print(i/b)
+			doc = etree.SubElement(root, 'text')
 			doc.text = t
 	tree = etree.ElementTree(root)
 	tree.write('./data/list_of_texts.xml')
+	
+	
+	
+	
+	
+#	root = etree.Element('texts')
+#	tree = etree.ElementTree(root)
+#	tree.write('./data/list_of_texts.xml')
+#	
+#	tree = xml.parse('./data/list_of_texts.xml')
+#	xmlRoot = tree.getroot()
+#	
+#	for i, t in enumerate(make_xml(c)):
+
+#		if i % int(b) == 0:
+#			print(i/int(b))
+#			doc = etree.SubElement(root, 'text')
+#			doc.text = t
+#	tree.write('./data/list_of_texts.xml')
