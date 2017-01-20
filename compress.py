93c60aa1fe3990922ec9dc5d0942f38d50ca825b
lishuo <lishuo98@yahoo.com.sg>
2017-01-20 14:51:58 +0800

    Minor changes

--- compress.py
+++ compress.py
@@ -1,1 +1,10 @@
 import bz2
+
+#a = bz2.BZ2File('./data/list_of_texts_compressed.xml.bz2', 'wb')
+
+#a.write()
+
+with open('./data/list_of_texts_compressed.xml.bz2', 'wb') as A:
+	with open('./data/list_of_texts.xml', 'rb') as B:
+		for data in B:
+			A.write(data)
