daa29f2490bd91de7d96eca84d611164525ced60
lishuo <lishuo98@yahoo.com.sg>
2017-02-02 14:15:00 +0800

    Something wrong in either lda.py or furthur_convert

--- furthur_convert_for_lda.py
+++ furthur_convert_for_lda.py
@@ -1,41 +1,52 @@
 from pprint import pprint
+import pickle
+#4134 unique tokens
 
-def maker(a_text):
+def adder(a_text):
 	b = ''.join(a_text)
-	texts = [text for text in b.split('\n')]
+	remove = ['']
+	texts = [text for text in b.split('\n') if text not in remove]
 	
-	texts = ' '.join(texts)
-	texts = texts.split('abcdefghijklmnopqrstuvwxyzzz')
-
 	document = []
-	for i in range(0, len(texts)-1):
+	for i in range(0, len(texts)):
 		a = texts[i].split()
 		document.append(a)
 	texts = document	#makes a list with the lines in the previous txt as sub lists
 
-
 	document = []
-	
 	i = 0
-	while i <= len(texts):
-		number = 0
-		number = int(texts[i][2])
+	while i < len(texts)-2:
+		try:
+			number = 0
+			number = int(texts[i][2])
+			position = i
+			while int(texts[position][0]) == int(texts[position+1][0]):
+				number += int(texts[position+1][2])
+				position += 1
+			i = position
+				
+		except:
+			pass
 		
-		if int(texts[i][0]) == int(texts[i+1][0]):
-			number += int(texts[i+1][2])
-			i+=1
-			
-		else:
-			i+=1
-			
-		a = number
-		appending = str(texts[i][0]) + '\t'+ str(texts[i][1]) +'\t' + str(number) +'\n'
-		document.append(appending)
-						
-	return document
-	#return res
+		document.append('{}\t{}\t{}'.format(str(texts[position][0]), str(texts[position][1]), str(number)))
+		
+		i += 1
 
+	print(i)
+	texts = document
+	document = []
+	for line in texts:
+		document.append(line.split('\t'))
+	texts = document
+	return texts
+	
 if __name__ == '__main__':
-	with open('./data/use_this_lda.txt', 'w') as w:
+	with open('./data/intermediate_lda.txt', 'wb') as w:
 		with open('./data/g_format_doc.txt', 'r') as read:
-			pprint(maker(read), stream = w)
+			pickle.dump(adder(read), w)
+	
+	with open('./data/use_this_lda.txt', 'w') as w:
+		with open('./data/intermediate_lda.txt', 'rb') as a:
+			read = pickle.load(a)
+			for i in range(0, len(read)-1):
+				w.write(str(read[i][0]) + '	' + str(read[i][1]) + '	' + str(read[i][2]) + '\n')
--- lda.py
+++ lda.py
@@ -1,8 +1,8 @@
 import gensim
 import bz2
+from time import time
 
-
-id2word = gensim.corpora.Dictionary.load_from_text('./data/test.txt')
+id2word = gensim.corpora.Dictionary.load_from_text('./data/use_this_lda.txt')
 
 
 
@@ -12,6 +12,9 @@ mm = gensim.corpora.MmCorpus('./datasets/the_corpus.mm') #Is fine
 print(mm)
 
 
-lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=100, update_every=0, passes=20)
+#lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=100, update_every=0, passes=20)
+
+lda = gensim.models.ldamodel.LdaModel(corpus=mm, id2word=id2word, num_topics=100, update_every=1, chunksize=10000, passes=1)
 
 print(lda)
+lda.print_topics(20)
