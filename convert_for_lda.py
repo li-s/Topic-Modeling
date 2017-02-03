8ca7181dfd89aa0f55fdb7e880f0c3bbcba5efe8
lishuo <lishuo98@yahoo.com.sg>
2017-02-03 15:41:13 +0800

    Speed issues

--- convert_for_lda.py
+++ convert_for_lda.py
@@ -1,5 +1,7 @@
 import pickle
+from time import time
 
+start1 = time()
 with open('./data/g_format_doc.txt', 'w') as w:
 	with open('./data/pruned_texts(byte).txt', 'rb') as a:
 		with open('./data/text_vectors.txt', 'rb') as b:
@@ -12,3 +14,4 @@ with open('./data/g_format_doc.txt', 'w') as w:
 					for n in range(len(texts[i])):
 						if texts[i][n][0] == dictionary[pairs]:	
 							w.write(str(dictionary[pairs]) + ' ' + pairs + ' '+ str(texts[i][n][1])+'\n')
+print('The program took: {}'.format(time()-start1))
--- prune.py
+++ prune.py
@@ -10,7 +10,7 @@ from nltk.stem.snowball import SnowballStemmer
 
 def remove(a_text, number):
 	texts = ' '.join(a_text)
-	remove = ('the, be, to, of, and, a, in, that, have, i, it, for, not, on, with, he, as, you, do, at, n, www, html, http, ref, org, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0'.split(', '))    #20 most common english words + web urls
+	remove = ('the, be, to, of, and, a, in, that, have, i, it, for, not, on, with, he, as, you, do, at, n, www, html, http, ref, org, was, is, s, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0'.split(', '))    #20 most common english words + web urls
 	remove_white_space = ['']
 	
 	texts = [word.lower() for word in re.split('\W', texts) if(word.lower() not in remove) 			and (word not in remove_white_space)]
@@ -56,11 +56,14 @@ if __name__ == '__main__':
 	with open('./data/pruned_texts.txt', 'w') as a:
 		with open('./data/extracted_texts.txt', 'r') as b:
 			pprint(remove(b, 1), stream = a)
-			
+	print('Step1: {}'.format(time()-start1))
+	
+	start2 = time()
 	with open('./data/pruned_texts(byte).txt', 'wb') as a:
 		with open('./data/extracted_texts.txt', 'r') as b:
 			pickle.dump(remove(b, 1), a)
-
+	print('Step2: {}'.format(time()-start2))
+	
 	with open('./data/text_vectors.txt', 'wb') as a:
 		with open('./data/extracted_texts.txt', 'r') as b:
 			pickle.dump(remove(b, 2), a)
